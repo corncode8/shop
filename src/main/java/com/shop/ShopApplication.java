5488e6dc97ad2fe4991fbb6e2c0c1fcda849c2ed
corncode8 <myeongcheol8@naver.com>
2023-06-12 13:03:25 +0900

    06121305

--- src/main/java/com/shop/ShopApplication.java
+++ src/main/java/com/shop/ShopApplication.java
@@ -2,18 +2,23 @@ package com.shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCrypt;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-public class ShopApplication {
-//	@Bean
+public class ShopApplication extends SpringBootServletInitializer {
+	//	@Bean
 //	public BCryptPasswordEncoder passwordEncoder() {
 //		return new BCryptPasswordEncoder();
 //	}
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ShopApplication.class);
+	}
 	@Bean   // 해당 메서드의 리턴되는 오브젝트를 IoC로 등록해줌
 	public BCryptPasswordEncoder encodePwd() {
 		return new BCryptPasswordEncoder();
